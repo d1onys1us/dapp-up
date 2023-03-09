c7c035ef0c558be19d0ede1661df40bc644afae2
dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-03-09 17:27:57 -0500

    fix dat bitttt

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
-  import ContractInteraction from "../components/ContractInteraction.svelte";
+  // import ContractInteraction from "../components/ContractInteraction.svelte";
 </script>
 
-{#if import.meta.env.MODE === "development"}
-  <ContractInteraction />
-{:else}
-  <h1>Just a home page.</h1>
-{/if}
+<!-- <ContractInteraction /> -->
+<h1>Just a home page.</h1>
