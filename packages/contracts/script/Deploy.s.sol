1da8f71c20110fb16b67f7323eeeb761837d0313
Kenk <kenghin_lim@hotmail.com>
2023-08-09 18:11:10 +0700

    update

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -9,8 +9,7 @@ import {BaseScript} from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcaster returns (Diamonds diamonds, Foo foo) {
+    function run() public broadcaster returns (Diamonds diamonds) {
         diamonds = new Diamonds();
-        foo = new Foo();
     }
 }
