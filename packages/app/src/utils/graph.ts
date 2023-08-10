cfbd42a29bf5a9ae11c98cb282f69f56d09db8d1
Kenk <kenghin_lim@hotmail.com>
2023-08-10 21:11:03 +0800

    Update packages/app/src/utils/graph.ts
    Co-authored-by: dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>

--- packages/app/src/utils/graph.ts
+++ packages/app/src/utils/graph.ts
@@ -1,6 +1,6 @@
 import { Client, cacheExchange, fetchExchange } from '@urql/svelte';
 
-const APIURL = ' http://43.153.69.251:8000/subgraphs/name/taiko/diamondsNFT'
+const API_URL = 'http://43.153.69.251:8000/subgraphs/name/taiko/diamondsNFT'
 
 export const client = new Client({
     url: APIURL,
